6530806d4fb9cb108ab57c40d2ffb601ee311964
VK2401 <vipvasanth433@gmail.com>
2023-09-15 19:34:34 +0530

    added theme color changer

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,21 @@ let cancelIcon = document.querySelector('.cancel-icon');
 let headerLinks = document.querySelector('.header-links');
 
 menuIcon.addEventListener('click', function () {
-    headerLinks.style.display = 'block';
-    headerLinks.style.right = '0';
-  });
-  
-  cancelIcon.addEventListener('click', function () {
-    
-    headerLinks.style.right = '-100vh';
-    headerLinks.style.display = 'none';
-  });
+  headerLinks.style.display = 'block';
+  headerLinks.style.right = '0';
+});
 
+cancelIcon.addEventListener('click', function () {
+
+  headerLinks.style.right = '-100vh';
+  headerLinks.style.display = 'none';
+});
+
+// theme color change script
+let themeInput = document.getElementById('theme');
+
+themeInput.addEventListener('change', function () {
+  // Get the value of the input element when it changes
+  const color = themeInput.value;
+  document.documentElement.style.setProperty('--primary-theme-color', color);
+});
